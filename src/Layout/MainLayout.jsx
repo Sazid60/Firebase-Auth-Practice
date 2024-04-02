adb9cfddc7e8865d7a594c4528aa66aa9172fcbd
Sazid60 <shahnawazsazid60@gmail.com>
2024-04-02 14:06:41 +0600

    Structure & Routes Created

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className='flex flex-col justify-center items-center'>
+            <Outlet></Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import MainLayout from './Layout/MainLayout';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Profile from './Components/Profile';
+import About from './Components/About';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children : [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+        <RouterProvider router={router} />
+  </React.StrictMode>,
+)
